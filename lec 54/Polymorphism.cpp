883b3e57bbfe210882a7903359a748f90f5bac51
PRASHIKGITHUB <prashik.bahadure5@gmail.com>
2024-08-21 22:08:43 +0530

    polymorphism updated lec54

--- lec 54/Polymorphism.cpp
+++ lec 54/Polymorphism.cpp
@@ -5,5 +5,13 @@ int main(){
     //one name multiple forms
     //eg: function overloading and operator overloading
     //eg: Virtual Functions
+
+    Ploymorphism in C++ can be of two types:
+    1. Compile time polymorphism
+        1.1 - Function overloading
+        1.2 - Operator overloading
+
+    2. Run time polymorphism
+        2.1 - Virtual functions
     return 0;
 }
